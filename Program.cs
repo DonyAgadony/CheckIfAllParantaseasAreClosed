ac23e7bc3b1c215ba3843213720c3f8a7e63d509
Daniel <donybof1@gmail.com>
2024-09-20 08:11:22 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    public static void Main()
+    {
+
+    }
+    public static bool IsBalanced(Stack<char> stack)
+    {
+        int countForward = 0;
+        int countBackwards = 0;
+        if (stack.Pop() != ')')
+        {
+            return false;
+        }
+        while (stack.Count > 1)
+        {
+            if (stack.Pop() == ')') { countBackwards++; }
+            else if (stack.Pop() == '(') { countForward++; }
+            else { return false; }
+        }
+        if (stack.Pop() != '(') { return false; }
+        return true;
+    }
+}
